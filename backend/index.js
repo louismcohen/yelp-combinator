b2d66cb1815d620134637f2ce0afb8edea8aeec7
d3lm3l <d3lm3l@gmail.com>
2021-04-21 22:04:32 -0700

    frontend folder placed inside backend

--- backend/index.js
+++ backend/index.js
@@ -21,9 +21,9 @@ app.use('/', yelpRouter);
 app.use('/', googleRouter);
 
 if (process.env.NODE_ENV == 'production') {
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('./frontend/build'));
   app.get('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
 
