79a0d6c37d2b43a614c6dcd64b8f6f37cb8ea1f3
d3lm3l <d3lm3l@gmail.com>
2021-12-25 15:45:10 -0800

    updated backend package.json

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/api/', googleRouter);
 app.use('/api/', geolocationRouter);
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
