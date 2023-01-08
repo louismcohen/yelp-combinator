2ccb9ca86e5b645a0777eb0d82289cdfc0319f4e
louismcohen <d3lm3l@gmail.com>
2023-01-08 13:22:37 -0500

    converted to react 18 createRoot

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import Map from './Map';
 import {Helmet} from "react-helmet";
@@ -30,11 +30,13 @@ Sentry.init({
     replaysOnErrorSampleRate: 1.0
   });
 
-ReactDOM.render( 
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render( 
     <React.StrictMode>
         <Map />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
